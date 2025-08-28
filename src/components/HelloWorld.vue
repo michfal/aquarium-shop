1d16ca2b6fa73e48e09ba1e729f21aca6fced103
Michał Fal <michfal86@gmail.com>
2025-08-28 15:26:18 +0200

    First commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = withDefaults(defineProps<{
+  msg?: string
+}>(), {
+  msg: 'testtest'
+})
+</script>
+
+<template>
+  <h1 class="green">{{ msg }}</h1>
+</template>
+
